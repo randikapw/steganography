a4a49c592c3e2f5f246e3789c0f8f7625d852643
Randika Weerasooriya <randikapw@gmail.com>
2024-05-18 21:27:40 +0530

    update code to get actual encrypted content

--- Only_for_Encoding_All.py
+++ Only_for_Encoding_All.py
@@ -63,8 +63,8 @@ def encrypt_and_hash_secret_message(secret_text):
     # Base64 encode the hashed secret for easier storage
     hashed_secret_base64 = base64.b64encode(hashed_secret).decode('utf-8')
     
-    # return encrypted_secret, hashed_secret_base64, key
-    return hashed_secret_base64
+    return encrypted_secret, hashed_secret_base64, key
+    # return hashed_secret_base64
 
 #Encode the encrypted secret text in the cover text
 def encode(hashed_secret_base64, open_text):
@@ -113,8 +113,8 @@ def main():
             print("An error occurred while detecting the language of the cover text. Please try again.")
             continue 
     secret_text = input("Enter the secret message to hide: ")
-
-    steganographed_text = encode(encrypt_and_hash_secret_message(secret_text), open_text) 
+    encrypted_secret, hashed_secret_base64, key = encrypt_and_hash_secret_message(secret_text)
+    steganographed_text = encode(encrypted_secret, open_text) 
         
   # Print the steganographed text
     print("Steganographed text:", steganographed_text)
