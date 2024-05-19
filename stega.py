6ad555182b5c875cb6f8c2bfaa8b46288bbda7ec
Randika Weerasooriya <randikapw@gmail.com>
2024-05-19 17:55:53 +0530

    new file

---
+++ stega.py
@@ -0,0 +1,19 @@
+
+def isNeedToEncode():
+    while True:
+    # Ask the user to enter the cover text in Sinhala
+        action = input("What do you want to do now? encode a text (E) or decode a text (D) ")
+        if action.upper() in ("E", "D"):
+            if action.upper() == "E":
+                return True
+            else:
+                return False
+        else:
+            print("Invalid action provided. Only supports E or D")
+
+def main():
+    print("Welcome to STEGA in සිංහල")
+    if (isNeedToEncode) :
+    
+    else:
+    
